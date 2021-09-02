4c1c54146e96598a03edc8d2eca369bc4b8223a6
Luis Felipe <l_felipelizeu@hotmail.com>
2021-09-01 23:51:25 -0300

    The MoviesList component loads all the available movies

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,7 +1,16 @@
 import Movie from './Movie.js';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function MoviesList ({ movies }) {
+export default function MoviesList () {
+    const [ movies, setMovies ] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/cineflex/movies")
+            .then((response) => setMovies(response.data));
+    }, []);
+
     return (
         <main>
             <h1>Selecione o filme</h1>
@@ -9,7 +18,7 @@ export default function MoviesList ({ movies }) {
                 {movies.map((movie,index) => {
                     return (
                         <Link to="/sessions">
-                            <Movie key={index} title={movie.title} img={movie.img} />
+                            <Movie key={index} title={movie.title} img={movie.posterURL} />
                         </Link>
                     );
                 })}
